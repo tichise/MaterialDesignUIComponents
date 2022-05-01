891627f66c037b1cb3b44fdb89fadb8df31ffb74
tichise <43707+tichise@users.noreply.github.com>
2022-05-01 11:20:37 +0900

    add shadowColor

--- Sources/MaterialDesignUIComponents/ViewModifier/Card/ElevatedCard.swift
+++ Sources/MaterialDesignUIComponents/ViewModifier/Card/ElevatedCard.swift
@@ -11,22 +11,25 @@ import SwiftUI
 /// https://m3.material.io/components/cards/overview
 public struct ElevatedCard: ViewModifier {
     
-    public init(cornerRadius: CGFloat) {
+    public init(cornerRadius: CGFloat, shadowColor: Color) {
         self.cornerRadius = cornerRadius
+        self.shadowColor = shadowColor
     }
     
     public var cornerRadius: CGFloat // 角丸の半径
+    public var shadowColor: Color
         
     public func body(content: Content) -> some View {
-        content.background(RoundedRectangleFill.init(cornerRadius: cornerRadius))
+        content.background(RoundedRectangleFill.init(cornerRadius: cornerRadius, shadowColor: shadowColor))
     }
 }
 
 /// 角丸の四角形
 private struct RoundedRectangleFill: View {
     var cornerRadius: CGFloat // 角丸の半径
+    var shadowColor: Color
 
     var body: some View {
-        return RoundedRectangle(cornerRadius: cornerRadius).foregroundColor(.white).shadow(color: ColorConstants.shadowColor, radius: 1, x: 1, y: 1)
+        return RoundedRectangle(cornerRadius: cornerRadius).foregroundColor(.white).shadow(color: shadowColor, radius: 1, x: 1, y: 1)
     }
 }
